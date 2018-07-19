6a82f78651b5589a25c86ee1d2d08db98f46734a
Mohammad Rajabloo <mrsoftware73@gmail.com>
2018-07-20 01:12:09 +0430

    Close right menu by default by setting menuOpen to false

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -15,7 +15,7 @@ class Dashboard extends Component {
     constructor(props) {
         super(props);
         this.state={
-            menuOpen:true
+            menuOpen:false
         }
     }
     menuHandler=()=>{
